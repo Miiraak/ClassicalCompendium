382c6c583ea78c1145860c14ec87dd721a33660e
Miiraak <miiraak@feynlok.ch>
2024-08-24 11:00:18 +0200

    Update unit tests for CodeCesar method in CryptoMethods
    Removed two assertions and added one new assertion in CryptoMethodsTests.cs. Ensured the remaining tests validate various inputs and shifts for the CodeCesar method.

--- ClassicalCompendium.Tests/CryptoMethodsTests.cs
+++ ClassicalCompendium.Tests/CryptoMethodsTests.cs
@@ -10,8 +10,7 @@ namespace ClassicalCompendium.Tests
             Assert.Equal("BCD", CryptoMethods.CodeCesar(1, "ABC"));
             Assert.Equal("BC DE", CryptoMethods.CodeCesar(1, "AB CD"));
             Assert.Equal("BC DE FG", CryptoMethods.CodeCesar(1, "AB CD EF"));
-            Assert.Equal("BC DE FG HI !!", CryptoMethods.CodeCesar(1, "AB CD EF GH !!"));
-            Assert.Equal("BC DE FG HI !! 234", CryptoMethods.CodeCesar(1, "AB CD EF GH !! 123"));     
+            Assert.Equal("BC DE FG HI !!", CryptoMethods.CodeCesar(1, "AB CD EF GH !!"));  
             Assert.Equal("A", CryptoMethods.CodeCesar(26, "A"));
             Assert.Equal("AB", CryptoMethods.CodeCesar(26, "AB"));
             Assert.Equal("ABC", CryptoMethods.CodeCesar(26, "ABC"));
@@ -19,6 +18,8 @@ namespace ClassicalCompendium.Tests
             Assert.Equal("AB CD EF", CryptoMethods.CodeCesar(26, "AB CD EF"));
             Assert.Equal("AB CD EF GH", CryptoMethods.CodeCesar(26, "AB CD EF GH"));
             Assert.Equal("AB CD EF GH !!", CryptoMethods.CodeCesar(26, "AB CD EF GH !!"));
+            Assert.Equal("2345678901", CryptoMethods.CodeCesar(1, "0123456789"));
+
         }
     }
 }
